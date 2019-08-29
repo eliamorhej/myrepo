1560fee0820a56f65bf08fa44fb181bc92b6f1ea
eliamorhej <54403842+eliamorhej@users.noreply.github.com>
2019-08-29 09:15:09 -0400

    eliamorhej
    updated file

--- Algorithm1.java
+++ Algorithm1.java
@@ -1,5 +1,5 @@
-/*purpose: first algortihm that we are going to make in part 3 of course
-intro to computer science 1. 
+/*purpose: first algortihm that we are going to make in part 3 of lab 02 
+of course intro to computer science 1. 
 programmer: elia morhej
 */
 
